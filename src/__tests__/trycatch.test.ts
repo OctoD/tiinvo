be8d129c0108df12ae51f05809a193becc96ead2
octod <iamoctod@gmail.com>
2021-03-16 10:31:53 +0100

    chore: adds test cases

--- src/__tests__/trycatch.test.ts
+++ src/__tests__/trycatch.test.ts
@@ -18,11 +18,36 @@ describe("trycatch", () => {
     expect(isErr(resulterr)).toBeTruthy();
   });
 
+  it(`trycatch example`, () => {
+    const thisthrows = () => { throw new Error(`This is Sparta`) }
+    const thisisok = (what: string) => `we love ${what}`
+    
+    const test1 = trycatch(thisthrows);
+    const test2 = trycatch(thisisok, 'kittens');
+    
+    expect(isErr(test1)).toBeTruthy();
+    expect(isOk(test2)).toBeTruthy();
+  });
+
   it("trycatchasync", async () => {
     const resultok = await trycatchAsync(fnokasync, 10);
     const resulterr = await trycatchAsync(fnerrasync, "ouch");
 
     expect(isOk(resultok)).toBeTruthy();
     expect(isErr(resulterr)).toBeTruthy();
+  });
+
+  it(`trycatchasync example`, async () => {
+    const fail = async () => { throw new Error('oops I did it again') }
+    const success = async (num: number) => num;
+    
+    const test1 = trycatchAsync(fail).then(isErr)       // Promise<true>
+    const test2 = trycatchAsync(success, 10).then(isOk) // Promise<true>
+
+    const res1 = await test1;
+    const res2 = await test2;
+
+    expect(res1).toBeTruthy();
+    expect(res2).toBeTruthy();
   });
 });
--- src/trycatch.ts
+++ src/trycatch.ts
@@ -1,7 +1,23 @@
 import * as result from "./result";
 
 /**
- *
+ * Calls a function safely and returns a `Result<T>` where `T` is the return type of that function.
+ * @since 2.0.0
+ * @example
+ * 
+ * ```ts
+ * import { trycatch, result } from 'tiinvo';
+ * 
+ * const thisthrows = () => { throw new Error(`This is Sparta`) }
+ * const thisisok = (what: string) => `we love ${what}`
+ * 
+ * const test1 = trycatch(thisthrows);
+ * const test2 = trycatch(thisisok, 'kittens');
+ * 
+ * result.isErr(test1)  // true
+ * result.isOk(test2)   // true
+ * 
+ * ```
  *
  * @template FnTry
  * @template K
@@ -21,8 +37,20 @@ export const trycatch = <FnTry extends (...args: K) => any, K extends any[]>(
 };
 
 /**
+ * Calls a function safely and returns a `Promise<Result<T>>` where `T` is the return type of that function.
+ * @since 2.0.0
  *
- *
+ * ```ts
+ * import { trycatchAsync, result } from 'tiinvo';
+ * 
+ * const fail = async () => { throw new Error('oops I did it again') }
+ * const success = async (num: number) => num;
+ * 
+ * trycatchAsync(fail).then(result.isErr)       // Promise<true>
+ * trycatchAsync(success, 10).then(result.isOk) // Promise<true>
+ * 
+ * ```
+ * 
  * @template FnTry
  * @template K
  * @param {FnTry} fn
