4e6ec1cd8f4cc171a74a8a2f0f1d33e3d0654d39
octod <iamoctod@gmail.com>
2021-04-02 17:22:37 +0200

    chore: adds rollup

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import typescript from 'rollup-plugin-typescript2';
+import {terser} from 'rollup-plugin-terser';
+
+export default [
+  // ES module build (replaces broken basic TypeScript compilation)
+  // * ref: <https://github.com/microsoft/TypeScript/issues/18442> , <https://github.com/alshdavid/rxjs/blob/main/rollup.config.js#L10>
+  // * ref: <https://github.com/microsoft/TypeScript/pull/35148>
+  // * ref: <https://github.com/microsoft/TypeScript/issues/37582>
+  {
+    preserveModules: true,
+    input: ['src/index.ts'],
+    output: [{ dir: 'dist/esm', format: 'esm', entryFileNames: '[name].mjs' }],
+    plugins: [typescript({ tsconfig: './tsconfig.build.json' })],
+  },
+  {
+    preserveModules: true,
+    input: ['src/index.ts'],
+    output: [{ dir: 'dist/cjs', format: 'cjs', entryFileNames: '[name].js' }],
+    plugins: [typescript({ tsconfig: './tsconfig.build.json' })],
+  },
+  {
+    preserveModules: false,
+    input: ['src/index.ts'],
+    output: [{ dir: 'dist/umd', format: 'umd', entryFileNames: '[name].js', name: 'tiinvo' }],
+    plugins: [typescript({ tsconfig: './tsconfig.build.json' }), terser()],
+  },
+  {
+    preserveModules: false,
+    input: ['src/index.ts'],
+    output: [{ dir: 'dist/amd', format: 'amd', entryFileNames: '[name].js', name: 'tiinvo' }],
+    plugins: [typescript({ tsconfig: './tsconfig.build.json' }), terser()],
+  },
+]
