503f5817c7994dac19668a0f4c7d85e7694ac890
octod <iamoctod@gmail.com>
2022-01-03 11:35:03 +0100

    feat: adds pipe.async

---
+++ src/__tests__/pipe.async.test.ts
@@ -0,0 +1,21 @@
+import { pipeasync, toasync } from '../pipe.async';
+import * as n from '../num';
+import * as m from '../maybe';
+
+describe(`pipe.async`, () => {
+  test(`async version of pipe`, async () => {
+    const wait = (ms: number) => (arg: string): Promise<string> => new Promise(resolve => setTimeout(() => resolve(arg), ms));
+
+    const piped = pipeasync(
+      toasync(n.uadd(10)),
+      toasync(n.umul(2)),
+      toasync(n.isEven),
+      toasync(m.mapOrElse(() => 'odd', () => 'even')),
+      wait(10),
+    );
+
+    expect(await piped(2)).toBe(`even`);
+    expect(await piped(1)).toBe(`even`);
+    expect(await piped(1.3)).toBe(`odd`);
+  })
+})
